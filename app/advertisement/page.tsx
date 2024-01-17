9df5a0c1f32ea26c8d09dc9f3a5c7276af0e18df
MatAIQUE <Matt.Tan@aiqueinnovation.com>
2024-01-17 17:28:58 +0800

    created configuration page

--- app/advertisement/page.tsx
+++ app/advertisement/page.tsx
@@ -12,20 +12,6 @@ const AdvertisementPage = () => {
           <div className="grid grid-cols-3 gap-4 mb-4 mx-2">
             <div className="col-span-3 h-auto rounded-xl bg-white dark:bg-gray-800 drop-shadow drop-shadow">
               <div className="py-4 px-6">
-                {/* <div className="flex justify-between items-center mb-4 grid">
-                    <h1 className="md:text-xl font-bold capitalize text-sm">Occupancy</h1>
-                    <div className="flex">
-                        <button className="md:p-4 hidden md:block hover:bg-secondary rounded-lg p-2">
-                            <FaDownload className="text-xs md:text-lg"/>
-                        </button>
-                        <div className="relative flex items-center">
-                        <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                            <FaSearch className="w-4 h-4 text-gray-500 dark:text-gray-400" />
-                        </div>
-                        <input type="text" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name, ID, Locker"/>
-                        </div>
-                    </div>
-                </div> */}
 
                 <div className="grid md:grid-cols-4 grid-cols-2 py-4 flex items-center py-4">
                   <div>
--- app/configuration/page.tsx
+++ app/configuration/page.tsx
@@ -1,4 +1,7 @@
+import { FaPlusCircle, FaEllipsisV, FaEllipsisH, FaUserCircle } from "react-icons/fa"
 import Nav from "../../components/ui/nav"
+import Image from "next/image"
+import SampleImage from "../../app/assets/img/kmc-ad-2.png"
 
 const ConfigurationPage = () => {
     return (
@@ -6,16 +9,121 @@ const ConfigurationPage = () => {
         <Nav/>
         
         <div className="p-2 pt-10 md:pt-10 sm:ml-64">
-            <div className="rounded-lg dark:border-gray-700">
-                <div className="grid grid-cols-3 gap-4 mb-4 mx-2">
-                    <div className="col-span-3 h-auto rounded-xl bg-white dark:bg-gray-800 drop-shadow drop-shadow">
-                        <div className="py-4 px-6">
-                            Content
+        <div className="rounded-lg dark:border-gray-700">
+          <div className="grid grid-cols-3 gap-4 mb-4 mx-2">
+            <div className="col-span-3 h-auto rounded-xl bg-white dark:bg-gray-800 drop-shadow drop-shadow">
+              <div className="py-4 px-6">
+
+                <div className="grid md:grid-cols-4 grid-cols-2 py-4 flex items-center py-4">
+                  <div>
+                    <h1 className="md:text-xl font-bold capitalize text-sm">Configuration</h1>
+                  </div>
+                  <div className="md:col-start-4 md:me-4 md:col-start-4 flex justify-end">
+                    <button className="btn rounded items-center p-2 md:p-4 text-white bg-primary flex hover:bg-primary/90 text-xs md:text-md">
+                        <FaPlusCircle className="text-sm"/>
+                        <p className="md:ml-3 text-xs ml-1">Add User</p>
+                    </button>
+                  </div>
+                </div>
+
+                {/* Content Table */}
+                <div className="flex w-full justify-start items-start">
+                  <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 w-full md:w-auto">
+                    <ul className="grid grid-cols-2">
+                      <li className="md:me-2">
+                        <a
+                          href="#"
+                          className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                        >
+                          Users
+                        </a>
+                      </li>
+                      <li className="md:me-2">
+                        <a
+                          href="#"
+                          className="inline-block p-4 text-primary border-b-2 border-primary rounded-t-lg dark:text-primary dark:border-blue-500"
+                          aria-current="page"
+                        >
+                          Pricing
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="w-full mt-2 grid grid-cols-3 gap-2 gap-y-2">
+                    <div className="col-span-3 md:col-span-1 mb-6">
+                        <div className="w-full flex flex-col justify-between sm:h-auto">
+                            <div className="grid grid-cols-4 bg-white md:drop-shadow-md rounded-xl p-4 relative flex justify-center">
+                                <div className="flex items-start mt-3 justify-center">
+                                    <FaUserCircle className="h-10 w-10"/>
+                                </div>
+                                <div className="grid ms-2 col-span-2">
+                                    <div>
+                                        <p className="font-bold w-full">Jane Hoe</p>
+                                        <p className="opacity-80 w-full hidden md:block">janehoe@biatch.com</p>
+                                    </div>
+                                    <div>
+                                        <p className="w-full mt-2">Admin</p>
+                                    </div>
+                                </div>
+                                <div className="absolute top-0 right-0 p-4">
+                                    <FaEllipsisV className="hidden md:block"/>
+                                    <FaEllipsisH className="md:hidden"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-span-3 md:col-span-1 mb-6">
+                        <div className="w-full flex flex-col justify-between sm:h-auto">
+                            <div className="grid grid-cols-4 bg-white md:drop-shadow-md rounded-xl p-4 relative flex justify-center">
+                                <div className="flex items-start mt-3 justify-center">
+                                    <FaUserCircle className="h-10 w-10"/>
+                                </div>
+                                <div className="grid ms-2 col-span-2">
+                                    <div>
+                                        <p className="font-bold w-full">Jane Hoe</p>
+                                        <p className="opacity-80 w-full hidden md:block">janehoe@biatch.com</p>
+                                    </div>
+                                    <div>
+                                        <p className="w-full mt-2">Admin</p>
+                                    </div>
+                                </div>
+                                <div className="absolute top-0 right-0 p-4">
+                                    <FaEllipsisV className="hidden md:block"/>
+                                    <FaEllipsisH className="md:hidden"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-span-3 md:col-span-1 mb-6">
+                        <div className="w-full flex flex-col justify-between sm:h-auto">
+                            <div className="grid grid-cols-4 bg-white md:drop-shadow-md rounded-xl p-4 relative flex justify-center">
+                                <div className="flex items-start mt-3 justify-center">
+                                    <FaUserCircle className="h-10 w-10"/>
+                                </div>
+                                <div className="grid ms-2 col-span-2">
+                                    <div>
+                                        <p className="font-bold w-full">Jane Hoe</p>
+                                        <p className="opacity-80 w-full hidden md:block">janehoe@biatch.com</p>
+                                    </div>
+                                    <div>
+                                        <p className="w-full mt-2">Admin</p>
+                                    </div>
+                                </div>
+                                <div className="absolute top-0 right-0 p-4">
+                                    <FaEllipsisV className="hidden md:block"/>
+                                    <FaEllipsisH className="md:hidden"/>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+              </div>
             </div>
+          </div>
         </div>
+      </div>
         </>
     )
 }
