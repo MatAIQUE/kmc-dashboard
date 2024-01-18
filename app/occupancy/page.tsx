55235726ad10757cc4da8f0b4b1c3ea83407ce19
MatAIQUE <Matt.Tan@aiqueinnovation.com>
2024-01-18 13:46:16 +0800

    added minor updates

--- app/occupancy/page.tsx
+++ app/occupancy/page.tsx
@@ -62,7 +62,7 @@ const OccupancyPage = () => {
                     <input
                       type="text"
                       id="input-group-1"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
+                      className="bg-gray-50 border text-gray-900 md:text-sm appearance-none rounded-lg focus:outline-none focus:outline-ring-primary focus:border-primary w-full ps-10 p-2.5"
                       placeholder="Name, Locker, ID"
                       />
                   </div>
