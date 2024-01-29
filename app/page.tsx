4b65e221a4c82ef6bb43eadfdde8c9e6fc3137d5
Tan Matt <matt.tan@aiqueinnovation.com>
2024-01-29 17:14:05 +0800

    Fixed error on path homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import Homepage from '../../kmc-dashboard/app/dashboard/page'
-import Image from 'next/image'
+import Homepage from "../app/dashboard/page";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <Homepage/>
-  )
+  return <Homepage />;
 }
