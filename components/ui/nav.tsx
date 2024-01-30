0795d6c30084c8bfd0ea4cbb0ad0c79ee633ac62
Tan Matt <matt.tan@aiqueinnovation.com>
2024-01-30 13:21:16 +0800

    refactor nav menu links

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -1,17 +1,46 @@
 "use client";
-import Toggle from "../toggle";
-import Image from "next/image";
-import DashboardIcon from "../../app/assets/icons/dashboard.svg";
-import OccupancyIcon from "../../app/assets/icons/occupancy.svg";
-import AdvertisementIcon from "../../app/assets/icons/ads.svg";
-import ConfigurationIcon from "../../app/assets/icons/configuration.svg";
-import SignOutIcon from "../../app/assets/icons/signout.svg";
-import Logo from "./logo";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { CiImageOn } from "react-icons/ci";
+import { FaCog } from "react-icons/fa";
+import { MdOutlineDashboard } from "react-icons/md";
+import { PiSignOut } from "react-icons/pi";
+import { RiArchiveDrawerLine } from "react-icons/ri";
+import Toggle from "../toggle";
+import Logo from "./logo";
+
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdOutlineDashboard />,
+      },
+      {
+        title: "Occupancy",
+        path: "/occupancy",
+        icon: <RiArchiveDrawerLine />,
+      },
+      {
+        title: "Advertisements",
+        path: "/advertisement",
+        icon: <CiImageOn />,
+      },
+      {
+        title: "Configuration",
+        path: "/configuration",
+        icon: <FaCog />,
+      },
+    ],
+  },
+];
 
 const Nav = () => {
   const router = useRouter();
+
+  const pathname = usePathname();
 
   return (
     <>
@@ -27,67 +56,21 @@ const Nav = () => {
               <Logo />
             </div>
 
-            <li>
-              <Link
-                href="/dashboard"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-              >
-                <Image
-                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  alt="Dashboard Icon"
-                  width={25}
-                  height={25}
-                  src={DashboardIcon}
-                />
-                <span className="ms-3">Dashboard</span>
-              </Link>
-            </li>
-
-            <li>
-              <Link
-                href="/occupancy"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-              >
-                <Image
-                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  alt="Occupancy Icon"
-                  width={25}
-                  height={25}
-                  src={OccupancyIcon}
-                />
-                <span className="ms-3">Occupancy</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/advertisement"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-              >
-                <Image
-                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  alt="Advertisement Icon"
-                  width={25}
-                  height={25}
-                  src={AdvertisementIcon}
-                />
-                <span className="ms-3">Advertisements</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/configuration"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-              >
-                <Image
-                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  alt="Configuration Icon"
-                  width={25}
-                  height={25}
-                  src={ConfigurationIcon}
-                />
-                <span className="ms-3">Configuration</span>
-              </Link>
-            </li>
+            {menuItems.map((cat, index) => (
+              <li key={index}>
+                {cat.list.map((item, itemIndex) => (
+                  <Link
+                    key={itemIndex}
+                    href={item.path}
+                    className={`flex items-center p-2 mb-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group
+                    ${pathname === item.path && "bg-gray-100"}`}
+                  >
+                    {item.icon}
+                    <span className="ms-3">{item.title}</span>
+                  </Link>
+                ))}
+              </li>
+            ))}
           </ul>
           <ul className="space-y-2 font-medium">
             <li>
@@ -96,13 +79,7 @@ const Nav = () => {
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 onClick={() => router.push("/")}
               >
-                <Image
-                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  alt="Dashboard Icon"
-                  width={25}
-                  height={25}
-                  src={SignOutIcon}
-                />
+                <PiSignOut />
                 <span className="ms-3 text-destructive">Signout</span>
               </a>
             </li>
