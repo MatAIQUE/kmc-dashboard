f5550277cc187a1b94fa8b7221ef12d4e77d42d1
ryosakakibara-aique <ryo.sakakibara@aiqueinnovation.com>
2024-02-13 14:15:28 +0800

    created accept invite page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,14 +5,6 @@ import { Input } from "../../components/ui/input"
 const LoginPage = () => {
     return (
         <>
-        <div className="p-2 grid hidden gap-y-2 w-full flex items-center px-[35%] h-screen pb-[25%]">
-            <div className="gap-y-2 grid flex items-center">
-                <h1>Login</h1>
-                <Input placeholder="password" type="password" className="w-full"/>
-                <Input placeholder="retype password" type="password" className="w-full"/>
-                <Button>Login</Button>
-            </div>
-        </div>
         <div className="flex items-center w-full justify-center h-screen pb-[25%] px-10 md:px-0">
             <div className="bg-white grid py-12 gap-y-2 px-10 md:w-[410px] w-full drop-shadow rounded-xl">
                 <h1 className="font-black text-xl text-left">LOG IN</h1>
