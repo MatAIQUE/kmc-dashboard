80fb7f11c0b9ede9794f5df0d4bb3031b6451184
ryosakakibara-aique <ryo.sakakibara@aiqueinnovation.com>
2024-02-14 11:26:02 +0800

    created form layout on occupancy/create

--- app/occupancy/create/page.tsx
+++ app/occupancy/create/page.tsx
@@ -24,6 +24,7 @@ import {
   CardHeader,
   CardTitle,
 } from "../../../components/ui/card";
+import { FaBuilding, FaChair } from "react-icons/fa";
 
 const formSchema = z.object({
   bookingId: z.string().min(7, {
@@ -60,14 +61,14 @@ const CreateLockerPage: React.FC = () => {
       <Nav />
       <div className="p-2 pt-10 md:pt-10 sm:ml-64">
         <div className="rounded-lg dark:border-gray-700">
-          <div className="grid grid-cols-3 gap-4 mb-4 mx-2">
+          <div className="grid flex justify-center gap-4 mb-4 mx-2">
             <Card className="w-[350px]">
               <CardHeader>
                 <CardTitle>Book a Locker</CardTitle>
                 <CardDescription></CardDescription>
               </CardHeader>
               <CardContent>
-                <Form {...form}>
+                {/* <Form {...form}>
                   <form
                     onSubmit={form.handleSubmit(onSubmit)}
                     className="space-y-8"
@@ -102,7 +103,67 @@ const CreateLockerPage: React.FC = () => {
                     />
                     <Button type="submit">Continue</Button>
                   </form>
-                </Form>
+                </Form> */}
+                <div className="grid gap-2 gap-y-6">
+                  <div
+                  // Center Content
+                  className="grid gap-2 gap-y-4"
+                  >
+                    <div
+                      // Service
+                      className="gap-y-2 grid"
+                    >
+                      <div className="flex font-bold text-xs">
+                        <h6 className="me-1">Service</h6>
+                        <span className="text-primary">*</span>
+                      </div>
+                      <div className="grid grid-cols-2 gap-2">
+                        <div className="outline outline-primary outline-1 rounded p-4 grid gap-y-2">
+                          <FaBuilding className="text-primary"/>
+                          <p className="font-semibold capitalize text-xs">
+                            Serviced Office
+                          </p>
+                        </div>
+                        <div className="outline outline-gray-300 outline-1 rounded p-4 grid gap-y-2">
+                          <FaChair />
+                          <p className="font-semibold capitalize text-xs">
+                            Coworking/Virtual
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      // Client Name
+                      className="gap-y-2 grid"
+                    >
+                      <div className="flex font-bold text-xs">
+                        <h6 className="me-1">Client Name</h6>
+                        <span className="text-primary">*</span>
+                      </div>
+                      <input
+                        placeholder="Client Name"
+                        className="text-xs outline outline-gray-300 outline-1 rounded p-2"
+                      />
+                    </div>
+                    <div
+                      // POC Contact
+                      className="gap-y-2 grid"
+                    >
+                      <div className="flex font-bold text-xs">
+                        <h6 className="me-1">POC Contact #</h6>
+                        <span className="text-primary">*</span>
+                      </div>
+                      <input
+                        placeholder="+63"
+                        className="text-xs outline outline-gray-300 outline-1 rounded p-2"
+                      />
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-2 gap-2">
+                    <button className="text-xs btn-secondary outline outline-gray-300 outline-1 p-2 rounded capitalize">Cancel</button>
+                    <button className="text-xs bg-primary text-white p-2 rounded capitalize">Continue</button>
+                  </div>
+                </div>
               </CardContent>
               {/* <CardFooter className="flex justify-between">
                 <Button variant="outline">Cancel</Button>
