0678f4f3c8a406b7442adc7c3aecca2db678b43c
Tan Matt <matt.tan@aiqueinnovation.com>
2024-02-27 13:18:02 +0800

    Fixed: Configuration change role displaying the actually values

--- app/configuration/page.tsx
+++ app/configuration/page.tsx
@@ -385,8 +385,8 @@ const ConfigurationPage = () => {
                                                                           field.onChange
                                                                         }
                                                                         defaultValue={
-                                                                          field.value
-                                                                        }
+                                                                          user.role
+                                                                        } // Assuming user.role holds the current role of the user
                                                                       >
                                                                         <FormControl>
                                                                           <SelectTrigger>
@@ -431,9 +431,11 @@ const ConfigurationPage = () => {
                                                                 type="submit"
                                                                 className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full my-0"
                                                               >
-                                                                {isLoading
-                                                                  ? <FaSpinner className="animate-spin"/>
-                                                                  : "Save Changes"}
+                                                                {isLoading ? (
+                                                                  <FaSpinner className="animate-spin" />
+                                                                ) : (
+                                                                  "Save Changes"
+                                                                )}
                                                               </button>
                                                             </div>
                                                           </form>
