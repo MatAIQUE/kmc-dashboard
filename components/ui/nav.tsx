e13757415500dde7b4c547a4c691971a903a60b5
ryosakakibara-aique <ryo.sakakibara@aiqueinnovation.com>
2024-02-27 15:25:24 +0800

    updated mobile view side bar - added avatar

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { CiImageOn } from "react-icons/ci";
-import { FaBasketballBall, FaCog } from "react-icons/fa";
+import { FaBasketballBall, FaCog, FaUser, FaUserCircle } from "react-icons/fa";
 import { MdOutlineDashboard } from "react-icons/md";
 import { PiSignOut } from "react-icons/pi";
 import { RiArchiveDrawerLine } from "react-icons/ri";
@@ -12,6 +12,8 @@ import { useState } from "react";
 import Image from "next/image";
 import MenuIcon from "../../app/assets/icons/menu.svg"
 import SignoutIcon from "../../app/assets/icons/signout.svg"
+import Avatar from "../../app/assets/icons/Avatar.svg"
+import { FaXmark } from "react-icons/fa6";
 
 const menuItems = [
   {
@@ -67,13 +69,31 @@ const showSidebar = () => {
         </div>
       <aside
         id="default-sidebar"
-        className={`fixed top-0 left-0 z-50 w-64 h-screen transition-transform sm:translate-x-0 ${!sidebarShown ? "-translate-x-full ":""}`}
+        className={`fixed top-0 left-0 z-50 md:w-64 w-full h-screen transition-transform sm:translate-x-0 ${!sidebarShown ? "-translate-x-full ":""}`}
         aria-label="Sidebar"
       >
         <div className="h-full px-3 py-4 flex flex-col justify-between bg-white drop-shadow dark:bg-gray-800">
           <ul className="space-y-2 font-medium">
-            <div className="w-full mb-10 hidden md:block">
+            <div className="w-full border-b-[1.5px] pb-4 flex items-center justify-between">
               <Logo />
+              <button className="p-4"  onClick={showSidebar}>
+                <FaXmark className="text-gray-500"/>
+              </button>
+            </div>
+            <div className="flex items-center md:hidden border-b-[1.5px] border-gray-300">
+              <div className="p-4">
+                <Image src={Avatar} alt="" width={42} height={42} />
+              </div>
+              <div className="grid">
+                <div>
+                  <h1 className="font-semibold text-lg">Jane Doe</h1>
+                </div>
+                <div>
+                  <p className="text-primary text-sm font-light">
+                    See your profile
+                  </p>
+                </div>
+              </div>
             </div>
 
             {menuItems.map((cat, index) => (
@@ -82,8 +102,8 @@ const showSidebar = () => {
                   <Link
                     key={itemIndex}
                     href={item.path}
-                    className={`flex items-center p-2 mb-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group
-                    ${pathname === item.path && "bg-gray-100" || 
+                    className={`flex items-center border-b-[1.5px] border-gray-300 px-2 py-4 md:border-0 mb-3 text-gray-900 md:rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group
+                    ${pathname === item.path && "md:bg-gray-100" || 
                       item.navStatus === "disabled" && "pointer-events-none opacity-30"
                   }`}
                   >
